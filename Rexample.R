08dea9042713bc5af551558459c499c85c25b68a
GValentini <GValentini@users.noreply.github.com>
2021-03-14 19:03:13 +0100

    Update Rexample.R

--- Rexample.R
+++ Rexample.R
@@ -11,3 +11,30 @@ ggplot(avocado, aes(year, region, fill = AveragePrice)) + geom_tile()
 ggplot(wine, aes(factor(quality), pH)) + 
   geom_violin() +
   labs(x = "quality", y = "pH")
+
+# The temp data, previewed below, records the average temperature for each of five cities across the year.
+# Create a plot to show the temp for each month and city.
+ggplot(city_temp, aes(month, city, fill = temp)) + 
+    geom_tile() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Add the text "AVG" to the plot near the new point to make it clear that the new point represents the average.
+highlight_avg <- fleet %>% 
+				 summarize(miles=mean(miles), cost=mean(cost))
+
+p <- ggplot(fleet, aes(x = miles, y = cost)) + 
+        geom_point(position = "jitter") + 
+        labs(y = "Cost per Mile ($)", x = "Miles driven per month") +
+	      geom_point(data = highlight_avg, aes(miles, cost), color = "red", size =  6)
+
+p +	annotate('text', 
+               x = highlight_avg$miles, 
+               y = highlight_avg$cost, 
+               label = 'AVG', size = 6, vjust = -1)
+
+# You have created a graphics object for you plot of number of searches over time for four different graphics tools.
+# Save a pdf version of this plot to the working directory.
+p <- ggplot(tools, aes(Week, Searches, group = Tool)) + geom_line()
+
+ggsave("searches_by_tool.pdf", p, device = "pdf")
+
